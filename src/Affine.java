77381db01b49951adbcbe5fc159e62ad3c062028
maya1108 <mayajenay94@gmail.com>
2017-08-09 14:11:04 -0400

    first commit

---
+++ src/Affine.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Affine {
+
+    public static void keyCheck(int alpha, int beta){
+        // checks if alpha and beta are within set parameters as ditacted by the cipher
+        if( alpha <1 || alpha >26 || beta <1 || beta>26 ){
+            System.out.println("Error key must have values greater than 0 and less than 26");
+            System.exit(0);
+        }
+        // need to add gcd check for alpha and 26
+    }
+
+    public static String affineCipher(String plaintext, int alpha, int beta){
+        // changed the plain text to upper case so that the alphabet can follow the 0-25 numbering system
+        plaintext =plaintext.toUpperCase();
+        String ciphertext = "";
+        for(int i = 0; i <= plaintext.length()-1; i++){
+            char letter = plaintext.charAt(i);
+            if (Character.isLetter(letter)){
+//              get the ascii value of the letter and minus it by 65 so it is now  number from 0-25
+                int ascii = (int)letter;
+                int p = ascii-65;
+//                perform the standard formual (ax+b mod m ) but add 65 so the letter is returned to a ascii value
+                int c = ((alpha*p +beta)%26) +65;
+                char l = (char)c;
+                ciphertext += l;
+            }
+            else{
+                ciphertext += letter ;
+            }
+        }
+        return ciphertext;
+    }
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a phrase to be encrpyted: ");
+        String text = reader.nextLine();
+        System.out.println("Enter alpha: ");
+        int alpha = reader.nextInt();
+        System.out.println("Enter beta: ");
+        int beta = reader.nextInt();
+        keyCheck(alpha,beta);
+        String cipher = affineCipher(text, alpha, beta);
+        System.out.println(cipher);
+    }
+}
+
---
+++ src/Ceaser.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class Ceaser {
+
+    public static String ceasarCipher(String plainText, int shift ){
+//        get the reminder of a shift if it is greater than the number of alphabets (26)
+        if(shift > 26) {
+            shift = shift % 26;
+        }
+//       if the shift is less than 0 get the reminder plus 26
+        else if (shift < 0){
+            shift = (shift%26)+26;
+        }
+        String cipherText = "";
+        for(int i = 0; i< plainText.length(); i++){
+            char letter = plainText.charAt(i);
+//        check if the character is a letter and not a space or punctuation or etc , if it is skip to else and just append to ciphertext
+            if(Character.isLetter(letter)){
+                if(Character.isLowerCase(letter)){
+                    char newLetter = (char) (letter + shift);
+                    if(newLetter >'z'){
+                        cipherText +=( char)(letter - (26 - shift));
+                    }
+                    else {
+                        cipherText += newLetter;
+                    }
+                }
+                if (Character.isUpperCase(letter)){
+                    char newLetter = (char) (letter + shift);
+                    if(newLetter >'Z'){
+                        cipherText +=( char)(letter - (26 - shift));
+                    }
+                    else {
+                        cipherText += newLetter;
+                    }
+                }
+            }
+            else {
+                cipherText += letter;
+            }
+        }
+        return cipherText ;
+    }
+    //same logic as encryption method but math is slightly different
+    public static String decrypt(String plainText, int shift ){
+        if(shift > 26) {
+            shift = shift % 26;
+        }
+        else if (shift < 0){
+            shift = (shift%26)+26;
+        }
+        String cipherText = "";
+        for(int i = 0; i<plainText.length(); i++){
+            char letter = plainText.charAt(i);
+            if(Character.isLetter(letter)){
+                if(Character.isLowerCase(letter)){
+                    char newLetter = (char) (letter - shift);
+                    if(newLetter < 'a'){
+                        cipherText +=( char)(letter - (26 - shift));
+                    }
+                    else {
+                        cipherText += newLetter;
+                    }
+                }
+                if (Character.isUpperCase(letter)){
+                    char newLetter = (char) (letter - shift);
+                    if(newLetter >'A'){
+                        cipherText +=( char)(letter - (26  + shift));
+                    }
+                    else {
+                        cipherText += newLetter;
+                    }
+                }
+            }
+            else {
+                cipherText += letter;
+            }
+        }
+        return cipherText ;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a phrase to be encrpyted: ");
+        String text = reader.nextLine();
+        System.out.println("Enter the number of shifts: ");
+        int shifts = reader.nextInt();
+        String cipher = ceasarCipher(text, shifts);
+        System.out.println(cipher);
+
+
+
+    }
+}
